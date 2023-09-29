da0d7f1dec1a7fa902b9c7ddec01c5f920f1d998
Rahma <rahmasassi12335@gmail.com>
2023-09-30 00:28:34 +0200

    commit front rahma

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -7,6 +7,12 @@ import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { SingupUserComponent } from './singup-user/singup-user.component';
+import { LoginComponent } from './login/login.component';
+import { DetailCarComponent } from './components/cars/detail-car/detail-car.component';
+import { SingupAgencyComponent } from './singup-agency/singup-agency.component';
+import { DetailReservationComponent } from './components/reservations/detail-reservation/detail-reservation.component';
+import { ConditionComponent } from './condition/condition.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +20,13 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     HeaderComponent,
     HomeComponent,
     FooterComponent,
-    DashboardComponent
+    DashboardComponent,
+    SingupUserComponent,
+    LoginComponent,
+    DetailCarComponent,
+    SingupAgencyComponent,
+    DetailReservationComponent,
+    ConditionComponent
   ],
   imports: [
     BrowserModule,
---
+++ Angular/src/app/singup-user/singup-user.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-singup-user',
+  templateUrl: './singup-user.component.html',
+  styleUrls: ['./singup-user.component.css']
+})
+export class SingupUserComponent {
+
+}
