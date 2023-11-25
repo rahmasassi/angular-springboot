03853488aec2c0f4ca31feef4d7390e0d809bdc0
rahma <rahmasassi12335@gmail>
2023-11-26 00:15:34 +0100

    update

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,21 @@
-variable vpc_cidr_block {}
-variable subnet_cidr_block {}
-variable avail_zone {}
-variable env_prefix {}
-variable my_ip {}
-variable instance_type {}
-variable region {}
+variable vpc_cidr_block {
+    default ="10.0.0.0/16"
+}
+variable subnet_cidr_block {
+    default ="10.0.10.0/24"
+}
+variable avail_zone {
+    default ="us-east-1a"
+}
+variable env_prefix {
+    default = "dev"
+}
+variable my_ip {
+    default ="0.0.0.0/0"
+}
+variable instance_type {
+    default ="t2.medium"
+}
+variable region {
+    default ="us-east-1"
+}
